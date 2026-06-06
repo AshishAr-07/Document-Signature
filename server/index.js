e6baee587cc6eb42ed8bcb196cee996298ab65a5
AshishAr-07 <rawatzon@gmail.com>
2026-06-06 22:43:41 +0530

    Auth system

--- server/index.js
+++ server/index.js
@@ -2,21 +2,34 @@ import express from 'express';
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from 'cors';
+import authRoutes from "./routers/auth.routes.js";
+import { connectDB } from './utils/dbconfig.js';
 
 const app = express();
 
 dotenv.config();
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+connectDB().then(() => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error, "Mongodb Connection Failed");
+  }
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.use("/api/v1/auth", authRoutes);
